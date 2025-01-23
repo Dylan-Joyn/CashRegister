22d3cf18fc21bbdb4534ed3d5e002a81002a4b0a
dylan <jdjjoyner@gmail.com>
2025-01-23 13:47:28 -0600

    test

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class MakingChange {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Making Change Calculator");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(new RegisterPanel());
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
 }
